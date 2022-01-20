5cfce3c80490537644ad4eb4f816844ced157563
pedroperrusi <pedro.perrusi@gmail.com>
2022-01-20 16:49:46 +0100

    [+] One away exercise added

---
+++ CtCI/cpp/Ch 1.Arrays And Strings/5. One Away/one_away_test.cpp
@@ -0,0 +1,30 @@
+#include <gtest/gtest.h>
+
+#include "one_away.h"
+
+#include <string>
+#include <vector>
+#include <iostream>
+
+using namespace std;
+
+
+TEST(CH1_ONE_AWAY, EMPTY_STRINGS) {
+	EXPECT_TRUE(oneAway("", ""));
+}
+
+TEST(CH1_ONE_AWAY, BIG_LENGTH_DIFFERENCE) {
+	EXPECT_FALSE(oneAway("", "hello world!"));
+	EXPECT_FALSE(oneAway("hello world!", ""));
+	EXPECT_FALSE(oneAway("hello worl", "hello world!"));
+}
+
+TEST(CH1_ONE_AWAY, REPLACEMENT) {
+	EXPECT_TRUE(oneAway("balloon", "yalloon"));
+	EXPECT_TRUE(oneAway("balloon", "balloof"));
+}
+
+TEST(CH1_ONE_AWAY, INSERTION_REMOVAL) {
+	EXPECT_TRUE(oneAway("balloon", "alloon"));
+	EXPECT_TRUE(oneAway("balloon", "balloo"));
+}
