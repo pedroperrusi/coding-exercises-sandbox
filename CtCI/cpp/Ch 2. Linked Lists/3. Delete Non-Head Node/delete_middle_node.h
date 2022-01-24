1927241e63eff9c06e8b48ac572b0b8a7e464b70
pedroperrusi <pedro.perrusi@gmail.com>
2022-01-24 13:00:58 +0100

    Added comments to CH2 exercise 3 solution

--- CtCI/cpp/Ch 2. Linked Lists/3. Delete Non-Head Node/delete_middle_node.h
+++ CtCI/cpp/Ch 2. Linked Lists/3. Delete Non-Head Node/delete_middle_node.h
@@ -3,8 +3,10 @@
 using namespace linked_lists;
 
 /**
- * @brief Trivial solution when you already know the size of the linked_list
- * In our case, the size function is O(N), so we need to loop through the list once more to return kth element
+ * @brief Deleting a node in the middle means its note the list head or tail node.
+ * It is trivial to delete it, since it always has a following neighboor.
+ * First copy its value and neighboor information, and then delete the neighboor.
+ * As we're using smart pointers for the next pointer it is pretty straightforward to avoid memory leaks
  * Time Complexity O(1)
  * Space Complexity O(1)
  */
